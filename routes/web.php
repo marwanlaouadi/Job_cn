31409f84f613005c852a56c93b53ae1c25f6d145
marwanlaouadi <marwanlaouadi@gmail.com>
2023-11-28 02:36:50 +0100

    marwan

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/connecte', function () {
 Route::redirect('/welcome', '/admin');
 Auth::routes(['register' => false]);
 
-//Route::get('/', 'HomeController@index')->name('home');
+Route::get('/welcome', 'HomeController@index')->name('welcome');
 Route::get('search', 'HomeController@search')->name('search');
 Route::resource('jobs', 'JobController')->only(['index', 'show']);
 Route::get('category/{category}', 'CategoryController@show')->name('categories.show');
